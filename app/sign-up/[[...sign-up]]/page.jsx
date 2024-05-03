93ae80ef8ca2e11bd9f171c25efefcddbfeac421
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-05-02 17:33:31 -0700

    clerk updated

--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -1,12 +1,5 @@
-import { SignIn } from "@clerk/nextjs";
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return (
-    <div className="w-full h-screen bg-red-500">
-      <div>test</div>
-      <div className="w-full h-screen flex justify-center align-middle">
-        <SignIn path="/sign-in" />
-      </div>
-    </div>
-  );
+  return <SignUp path="/sign-up" />;
 }
