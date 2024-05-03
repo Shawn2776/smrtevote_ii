565a8f39b44e9eb485b0c619bd1fe8ccd0e8eba0
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-05-02 17:17:37 -0700

    added webhook programming

--- middleware.js
+++ middleware.js
@@ -1,6 +1,14 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publicRoutes: [
+    "/",
+    "/sign-in",
+    "/sign-up",
+    "/forgot-password",
+    "/api/webhooks/clerk",
+  ],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
